06e66e2d18729574e62e9e781866b28d79617aba
phher802 <pher024@gmail.com>
2021-06-29 22:36:24 -0500

    Add test to return 201 HTTP response for POST method

--- test/SkinCareAPI.Tests/SkinCaresControllerTests.cs
+++ test/SkinCareAPI.Tests/SkinCaresControllerTests.cs
@@ -202,5 +202,25 @@ namespace SkinCareAPI.Tests
             Assert.IsType<ActionResult<SkinCareReadDto>>(result);
         }
 
+        [Fact]
+        public void CreateSkinCare_Returns201Created_WhenValidObjectSubmitted()
+        {
+        //Arrange
+            mockRepo.Setup(repo => repo.GetSkinCareById(1)).Returns(new SkinCare{
+                Id = 1,
+                IngredientName = "mock",
+                Contain = "mock",
+                ReasonWhyItsBad = "mock",
+                Source = "mock"});
+
+                var controller = new SkinCaresController(mockRepo.Object, mapper);
+        
+        //Act
+            var result = controller.CreateSkincare(new SkinCareCreateDto{});
+        
+        //Assert
+            Assert.IsType<CreatedAtRouteResult>(result.Result);
+        }
+
     }
 }
